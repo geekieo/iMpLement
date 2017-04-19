439622471e13acd6d1565ca289e798ff8b1f60d1
geekieo <geekieo@hotmail.com>
2017-04-19 13:24:35 +0800

    绘制飞船

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,15 +3,21 @@
 import sys
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 
 def run_game():
 
     # 初始化游戏并创建屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    # 设置背景色
-    bg_color = (230, 230, 230)
+
+    #创建一艘飞船
+    ship = Ship(screen)
 
     # 开始游戏主循环
     while True:
@@ -20,10 +26,13 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
         # 每次循环时都重绘屏幕
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+
         # 让最近的屏幕可见
         pygame.display.flip()
         # 延迟 33 毫秒执行下以循环，限制最高帧数为 30 FPS
         pygame.time.delay(33)
+
 
 run_game()
