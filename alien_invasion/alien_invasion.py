48b7105f5be8134c0814fd7fcc497876eb64ca48
geekieo <geekieo@hotmail.com>
2017-04-23 00:42:47 +0800

    代码调整

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -7,6 +7,7 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 
+
 def run_game():
 
     # 初始化游戏并创建屏幕对象
@@ -16,12 +17,12 @@ def run_game():
                                       ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    #创建一艘飞船
+    # 创建一艘飞船
     ship = Ship(screen)
 
     # 开始游戏主循环
     while True:
-        gf.check_events
+        gf.check_events()
         gf.update_screen(ai_settings, screen, ship)
         # 让最近的屏幕可见
         pygame.display.flip()
--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -6,15 +6,15 @@
 import sys
 import pygame
 
+
 def check_events():
     '''响应按键和鼠标事件'''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
+
 def update_screen(ai_settings, screen, ship):
-    '''更新图像，并绘制到屏幕''' 
+    '''更新图像，并绘制到屏幕'''
     screen.fill(ai_settings.bg_color)
     ship.blitme()
-
-            
