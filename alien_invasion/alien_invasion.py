26acb710ec1c4f4c6a0356708b3f3ed2c41dd535
geekieo <geekieo@hotmail.com>
2017-04-23 00:31:38 +0800

    重构：增加模块game_functions

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -5,7 +5,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
-
+import game_functions as gf
 
 def run_game():
 
@@ -21,14 +21,8 @@ def run_game():
 
     # 开始游戏主循环
     while True:
-        # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        # 每次循环时都重绘屏幕
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
+        gf.check_events
+        gf.update_screen(ai_settings, screen, ship)
         # 让最近的屏幕可见
         pygame.display.flip()
         # 延迟 33 毫秒执行下以循环，限制最高帧数为 30 FPS
---
+++ alien_invasion/game_functions.py
@@ -0,0 +1,20 @@
+'''
+事件代码，
+隔离事件，管理循环
+归纳 alien_invasion，清晰代码逻辑
+'''
+import sys
+import pygame
+
+def check_events():
+    '''响应按键和鼠标事件'''
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+def update_screen(ai_settings, screen, ship):
+    '''更新图像，并绘制到屏幕''' 
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+            
