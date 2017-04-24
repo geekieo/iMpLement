6bd00020da3742b732bc4f62cb6e36618d064023
geekieo <geekieo@hotmail.com>
2017-04-25 05:35:58 +0800

    重构 check_events()

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -7,6 +7,7 @@ import pygame
 
 
 def check_keydown_events(event, ship):
+    '''响应按键按下'''
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
@@ -17,6 +18,7 @@ def check_keydown_events(event, ship):
         ship.moving_down = True
 
 def check_keyup_events(event, ship):
+    '''响应按键松开'''
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
     elif event.key == pygame.K_LEFT:
