500c220a8b1e2707dff021f9214ad91da858e879
geekieo <geekieo@hotmail.com>
2017-04-28 00:29:11 +0800

    多继承

---
+++ learnPy/class/super.py
@@ -0,0 +1,34 @@
+'''multiple inheritance'''
+class A(object):
+    def __init__(self):
+        print("enter A")
+        print("leave A")
+class B(object):
+    def __init__(self):
+        print("enter B")
+        print("leave B")
+class C(A):
+    def __init__(self):
+        print("enter C")
+        super(C, self).__init__()
+        print("leave C")
+class D(A):
+    def __init__(self):
+        print("enter D")
+        super(D, self).__init__()
+        print("leave D")
+class E(B, C):
+    def __init__(self):
+        print("enter E")
+        B.__init__(self)
+        C.__init__(self)
+        print("leave E")
+class F(E, D):
+    def __init__(self):
+        print("enter F")
+        E.__init__(self)
+        D.__init__(self)
+        print("leave F")
+
+
+f = F()
