65be5caf85ae56bd0751f1aeda02ab014b51a367
geekieo <geekieo@hotmail.com>
2017-05-15 20:43:15 +0800

    kNNf分类器

--- ml_algorithm/knn/kNN.py
+++ ml_algorithm/knn/kNN.py
@@ -12,12 +12,32 @@ Output: the most popular(has more neighbors) class label
 from numpy import *
 import operator
 
-# create a dataset which contains 4 samples with 2 classes  
+# create a dataset which contains 4 samples with 2 classes
+
+
 def createDataSet():
     # create a matrx: each row as a sample
     group = array([[1.0, 0.9], [1.0, 1.0], [0.1, 0.2], [0.0, 0.1]])
-    labels = ['A','A','B','B']
+    labels = ['A', 'A', 'B', 'B']
     return group, labels
 
 # classify using kNN
-def kNNClassify(newinput, dataSet, labels, k):
+
+
+def kNNClassify(inData, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    # 1 距离计算 三行
+    diffMat = tile(inData, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    # 2 选择距离最小的k个点 两行
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    sortedClassCount = sorted(classCount.items(),
+                              # 3 排序
+                              key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
