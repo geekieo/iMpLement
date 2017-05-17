6fca45c3042f36777823a948c1685ddd2a9cb997
geekieo <geekieo@hotmail.com>
2017-05-18 03:30:53 +0800

    modify annotation

--- ml_algorithm/knn/kNN.py
+++ ml_algorithm/knn/kNN.py
@@ -24,22 +24,22 @@ def createDataSet():
 # classify using kNN 分类器实现
 
 
-def kNNClassify(inData, dataSet, labels, k):
+def kNNClassify(inputData, dataSet, labels, k):
     # the number of row
     dataSetSize = dataSet.shape[0]
     # 1 calculate Euclidean distance
     # tile(A,(x,y)) A沿各个维度复制x或y次,x为行，y为列
     # sum(axis=1) 行内求和，按照行的方向相加,sum(axis=0) 列内求和，按照列的方向相加，sum()全部相加  
-    diffMat = tile(inData, (dataSetSize, 1)) - dataSet  # subtract element-wise
+    diffMat = tile(inputData, (dataSetSize, 1)) - dataSet  # subtract element-wise
     sqDiffMat = diffMat**2                      # squared for the subtract
     sqDistances = sqDiffMat.sum(axis=1)         # sum is performed by row 
-    distances = sqDistances**0.5
+    distances = sqDistances**0.5                # distances from inputData to each point in dataSet
     # 2 sort the distance  两行
-    # argsort() returns the indices that would sort an array in a ascending order 返回矩阵升序排序的索引mat
-    sortedDistIndicies = distances.argsort()
+    # argsort() returns the indices that would sort an array in a ascending order 
+    sortedDistIndicies = distances.argsort() #返回矩阵升序排序的索引,如 array([5,2,4])返回 array([3,1,2])
     classCount = {}
     for i in range(k):
-        # 3 choose the min k distance
+        # 3 choose the label of the min k distance
         voteIlabel = labels[sortedDistIndicies[i]]
         # 4 count the times labels occur
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
