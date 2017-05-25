1418b368155c5ee43788373f1b261725e84dfd60
geekieo <geekieo@hotmail.com>
2017-05-26 04:55:04 +0800

    add datingClassTest()

--- ml_algorithm/knn/test_datingData.py
+++ ml_algorithm/knn/test_datingData.py
@@ -1,14 +1,33 @@
-import kNN  
+from kNN import *
 
-# 作图展示
-datingMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
-x=datingMat[:,0]
-y=datingMat[:,1]
-z=datingMat[:,2]
-kNN.drawPlot3D(x,y,z,datingLabels)
-# 归一化
-normDatingMat,ranges,minVals = kNN.autoNorm(datingMat)
-x=normDatingMat[:,0]
-y=normDatingMat[:,1]
-z=normDatingMat[:,2]
-kNN.drawPlot2D(x,y,datingLabels)
+
+def drawPlot():
+    # 作图展示
+    datingMat, datingLabels = file2matrix('datingTestSet2.txt')
+    x = datingMat[:, 0]
+    y = datingMat[:, 1]
+    z = datingMat[:, 2]
+    drawPlot3D(x, y, z, datingLabels)
+    # 归一化
+    normMat, ranges, minVals = autoNorm(datingMat)
+    x = normMat[:, 0]
+    y = normMat[:, 1]
+    z = normMat[:, 2]
+    drawPlot2D(x, y, datingLabels)
+
+
+def datingClassTest():
+    hoRatio = 0.1
+    datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :],
+                                     datingLabels[numTestVecs:m], 3)
+        print('the classifier came back with: %d, the real answer is: %d' %
+              (classifierResult, datingLabels[i]))
+        if (classifierResult != datingLabels[i]):
+            errorCount += 1.0
+    print('the total error rate is %f' % (errorCount / float(numTestVecs)))
