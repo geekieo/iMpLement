d411a993c53e78e46344232c317f5f598cdf728d
geekieo <geekieo@hotmail.com>
2017-05-26 14:53:01 +0800

    约会分类测试方法修改

--- ml_algorithm/knn/test_datingData.py
+++ ml_algorithm/knn/test_datingData.py
@@ -24,10 +24,12 @@ def datingClassTest():
     numTestVecs = int(m * hoRatio)
     errorCount = 0.0
     for i in range(numTestVecs):
-        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :],
+        classifierResult = kNNClassify(normMat[i, :], normMat[numTestVecs:m, :],
                                      datingLabels[numTestVecs:m], 3)
         print('the classifier came back with: %d, the real answer is: %d' %
               (classifierResult, datingLabels[i]))
         if (classifierResult != datingLabels[i]):
             errorCount += 1.0
     print('the total error rate is %f' % (errorCount / float(numTestVecs)))
+
+datingClassTest()
