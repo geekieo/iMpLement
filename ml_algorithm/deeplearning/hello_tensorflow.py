a3fba165ffc5c84394687b84308278a9f3fd7171
geekieo <geekieo@hotmail.com>
2017-06-11 02:19:41 +0800

    mat multiple

--- ml_algorithm/deeplearning/hello_tensorflow.py
+++ ml_algorithm/deeplearning/hello_tensorflow.py
@@ -1,6 +1,16 @@
 import tensorflow as tf
 hello = tf.constant('Hello, Tensorflow!')
 sess = tf.Session()
-a = tf.constant(10)
-b = tf.constant(32)
-print(sess.run(a + b))
+x1 = tf.constant(10)
+x2 = tf.constant(32)
+print(sess.run(x1 + x2))
+
+# creates a graph
+a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+c = tf.matmul(a, b)
+# creates a session with log_device_placement set to True
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# runs the op
+print("a=",a,"\n")
+print(sess.run(c))
