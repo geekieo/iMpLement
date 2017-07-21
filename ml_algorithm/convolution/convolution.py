d080affd399ba9cc59e9faad9ef172768504b90f
geekieo <geekieo@hotmail.com>
2017-07-21 21:29:01 +0800

    traverse test

--- ml_algorithm/convolution/convolution.py
+++ ml_algorithm/convolution/convolution.py
@@ -10,24 +10,23 @@ class Convolution(object):
         '''
         #获取 kernal, src 长宽
         kRows = len(kernal)
-        kRRad = int(kRows/2) #半径
+        kRRad = int(kRows / 2)  #半径
         kCols = len(kernal[0])
-        kCRad = int(kCols/2) #半径
+        kCRad = int(kCols / 2)  #半径
         sRows = len(src)
         sCols = len(src[0])
 
         #按 src 下标遍历
-        for sRIndex,sRow in enumerate(src):
+        for sRIndex, sRow in enumerate(src):
             #卷积时kernal边界不可超越src边界
-            if((sRIndex-kRRad)>0 and (sRIndex+kRRad)<sRows ):
-                for sCIndex,sPixel in enumerate(sRow):
+            if ((sRIndex - kRRad) >= 0 and (sRIndex + kRRad) <= sRows - 1):
+                for sCIndex, sPixel in enumerate(sRow):
                     #卷积时kernal边界不可超越src边界
-                    if((sCIndex - kCRad)>0 and (sCIndex+kCRad)<sCols ):
+                    if ((sCIndex - kCRad) >= 0 and
+                        (sCIndex + kCRad) <= sCols - 1):
                         #按 kernal 下标遍历
-                        for cRIndex,kRow in enumerate(kernal):
-                            for cCIndex,kPixel in enumerate(kRow):
+                        for cRIndex, kRow in enumerate(kernal):
+                            for cCIndex, kPixel in enumerate(kRow):
                                 pass
                         print(sPixel)
-                print("\n")        
-
-
+                print("\n")
