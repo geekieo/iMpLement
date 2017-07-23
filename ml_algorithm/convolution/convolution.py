600b70bdd0b9aa370fe5ac716e092c7394712215
geekieo <geekieo@hotmail.com>
2017-07-23 17:31:27 +0800

    二维矩阵卷积运算

--- ml_algorithm/convolution/convolution.py
+++ ml_algorithm/convolution/convolution.py
@@ -2,31 +2,43 @@ class Convolution(object):
     '''
     二维数组卷积运算
     '''
-
     def convolution(self, kernal, src):
         '''
         kernal 为卷积算子矩阵,\n
         src 为被卷积矩阵
         '''
         #获取 kernal, src 长宽
-        kRows = len(kernal)
-        kRRad = int(kRows / 2)  #半径
-        kCols = len(kernal[0])
-        kCRad = int(kCols / 2)  #半径
-        sRows = len(src)
-        sCols = len(src[0])
-
+        kRowLen = len(kernal)
+        kRRad = int(kRowLen / 2)  #半径
+        kColLen = len(kernal[0])
+        kCRad = int(kColLen / 2)  #半径
+        kSize = kRowLen * kColLen
+        sRowLen = len(src)
+        sColLen = len(src[0])
+        #初始化结果矩阵
+        dstRows = sRowLen-2*kRRad
+        dstCols = sColLen-2*kCRad
+        dst = [[0 for col in range(dstCols)]for row in range(dstRows)]
         #按 src 下标遍历
         for sRIndex, sRow in enumerate(src):
+            #在src上遍历，行的范围
+            minPixY = sRIndex - kRRad
+            maxPixY = sRIndex + kRRad
             #卷积时kernal边界不可超越src边界
-            if ((sRIndex - kRRad) >= 0 and (sRIndex + kRRad) <= sRows - 1):
+            if (minPixY >= 0 and maxPixY <= sRowLen - 1):
                 for sCIndex, sPixel in enumerate(sRow):
+                    #在 src 上遍历，列的范围
+                    minPixX = sCIndex - kCRad
+                    maxPixX = sCIndex + kCRad
                     #卷积时kernal边界不可超越src边界
-                    if ((sCIndex - kCRad) >= 0 and
-                        (sCIndex + kCRad) <= sCols - 1):
-                        #按 kernal 下标遍历
-                        for cRIndex, kRow in enumerate(kernal):
-                            for cCIndex, kPixel in enumerate(kRow):
-                                pass
-                        print(sPixel, end='')
-                print(end='\n')
+                    if (minPixX >= 0 and maxPixX <= sColLen - 1):
+                        dstPixel = 0
+                        # 按 kernal 卷积
+                        for kRIndex, kRow in enumerate(kernal):
+                            for kCIndex, kPixel in enumerate(kRow):
+                                dstPixel += kPixel * src[minPixY + kRIndex][minPixX + kCIndex]
+                        dst[sRIndex - kRRad][sCIndex - kCRad] = dstPixel
+        print(dst)
+
+    def transpose(self, src):
+        '''矩阵转置'''
