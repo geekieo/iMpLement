0fa74fbe81335112346a884d44d543166cae9556
geekieo <geekieo@hotmail.com>
2017-08-04 15:05:05 +0800

    recode finish

--- learnPy/search/manacher.py
+++ learnPy/search/manacher.py
@@ -5,25 +5,48 @@ core princple：
     else 在字符串长度范围内搜索回文
 """
 def manacher(s):
-    radii=[-1]*len(s) #存储每个字符的回文半径
-    maxRadius = 0 #最长回文右边界
-
     # step1: even to odd,  
     # princple: k -> 2k-1，
-    # description: add '#' after every char, delete the last one，
+    # description: add '#' between each char, the count will always be odd
     s = '#'.join(s)
-    print(s)
-    print(radii)
+
+    # step2: search palindorme 
+    lenth = len(s)
+    radii=[-1]*lenth #存储每个字符的回文半径，初始化为-1表示未赋值
+    posCenter = 0 #最右边界回文中心
+    posRadius = 0 #最右边界回文半径
+
+    for i in range(lenth):
+        radii[i]=0 #初始和回文半径为0
+        iMirror = 2*posCenter - i
+        if i < posRadius:
+            # 下面四句为 radii[i]=min(radii[iMirror], posRadius - i)
+            if i+radii[iMirror] <= posRadius:
+                radii[i] = radii[iMirror]
+            else:
+                radii[i] = posRadius - i
+        #尝试扩展 i 点的回文半径，注意判断边界
+        iLeft = radii[i]+1 #初始化扩展半径
+        while i-iLeft>0 and i+iLeft <lenth-1 and s[i+iLeft]==s[i-iLeft]:
+            radii[i]+=1
+            iLeft +=1
+        #更新最长回文中心和边界
+        if radii[i]+i>posRadius:
+            posCenter = i
+            posRadius = radii[i]
+    return radii
+
 
 
 def test():
     s ="ajafajafb"
-    manacher(s)
-    # RL = manacher(s)
-    # newRL= []
-    # for i in range(len(RL)):
-    #     if i%2==1:
-    #         newRL.append(RL[i])
-    # print(newRL)
+    RL = manacher(s)
+    newRL= []
+    
+    for i in range(len(RL)):
+        if i%2==0:
+            newRL.append(RL[i])
+    #
+    print(newRL)
 
 test()
