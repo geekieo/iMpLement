4d0b5e47c4de9886d62e4d89216a7cf6c69326ee
geekieo <geekieo@hotmail.com>
2017-08-16 13:18:17 +0800

    按多列排序

--- learnPy/sort/sort.py
+++ learnPy/sort/sort.py
@@ -1,6 +1,8 @@
 nums = [3, 2, 8, 0, 1]
 print(sorted(nums))
 
+################################
+
 score = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
 
 
@@ -13,3 +15,11 @@ def by_name(t):
 
 
 print(sorted(score, key=by_score))
+
+#####################
+
+elem = [(2,11,"A"),(1,11,"D"),(1,3,"C"),(2,4,"B")]
+print(elem)
+print(sorted(elem))
+# lambda 函数返回新的元组
+print(sorted(elem,key = lambda e:(e[1],e[0])))
