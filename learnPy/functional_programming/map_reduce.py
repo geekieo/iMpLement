e807e87c31e424d0f76281eaabf1dbc5ed67edc3
geekieo <geekieo@hotmail.com>
2017-08-16 10:51:35 +0800

    map 为一元操作函数；reduce 为二元操作函数

--- learnPy/functional_programming/map_reduce.py
+++ learnPy/functional_programming/map_reduce.py
@@ -8,11 +8,12 @@ def rf(x, y):
     return x * y
 
 
-# map 函数参数仅接受 1 个参数
+# map 函数参数仅接受 1 个参数，一元操作函数
 m = map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 print(list(m))
 
+# python3 中 reduce 已不再是内建函数，需 import
 from functools import reduce
-# reduce 函数参数需有s两个形参
+# reduce 函数参数必须有 2 个形参，二元操作函数
 r = reduce(rf, [1, 3, 5, 7, 9])
 print(r)
