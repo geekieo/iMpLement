0c6d44907d90fbfe49f6b37336013cf63a532d1d
geekieo <geekieo@hotmail.com>
2017-08-16 10:43:55 +0800

    reduce

--- learnPy/functional_programming/map_reduce.py
+++ learnPy/functional_programming/map_reduce.py
@@ -1,9 +1,18 @@
 def f(x):
+    # 一个形参
     return x * x
 
 
-r = map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-print(list(r))
+def rf(x, y):
+    # 两个形参
+    return x * y
 
-def normalize(name):
-    
+
+# map 函数参数仅接受 1 个参数
+m = map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+print(list(m))
+
+from functools import reduce
+# reduce 函数参数需有s两个形参
+r = reduce(rf, [1, 3, 5, 7, 9])
+print(r)
