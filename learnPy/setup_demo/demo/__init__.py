d798b16a0b9d9906eba48f38df93cb01b7d48e49
geekieo <geekieo@hotmail.com>
2017-08-27 18:18:03 +0800

    setuptools sample

---
+++ learnPy/setup_demo/demo/__init__.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+#-*- coding：utf-8 -*-
+
+def test():
+    print("hello world")
+
+if __name__=='__main__':
+    test()
---
+++ learnPy/setup_demo/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name = "setup_demo",
+    version = "0.1",
+    packages = find_packages(),
+    
+    author = "geekieo",
+    email = "geekieo@hotmail.com",
+)
