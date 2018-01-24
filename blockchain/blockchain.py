8d89630fa335c39c3f16ee78133901616a9e65bb
geekieo <geekieo@hotmail.com>
2018-01-24 16:06:59 +0800

    注册两个节点到API上，一个是添加相邻节点，另一个是处理冲突

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -187,6 +187,7 @@ class Blockchain(object):
 
         return proof
 
+
     @staticmethod
     def valid_proof(last_proof, proof):
         """
@@ -291,6 +292,47 @@ def full_chain():
         'length': len(blockchain.chain),
     }
     return jsonify(response), 200
+
+"""
+添加相邻节点
+"""
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+"""
+处理冲突
+"""
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
