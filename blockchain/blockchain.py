3b0cddb8b82dd71c0046ab42a2ee55015d38e747
geekieo <geekieo@hotmail.com>
2018-01-24 14:42:29 +0800

    debug

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,10 +1,9 @@
 import hashlib
 import json
-from textwrap import dedent
 from time import time
 from uuid import uuid4
-from flask import Flask
 
+from flask import Flask, jsonify, request
 
 class Blockchain(object):
 	def __init__(self):
