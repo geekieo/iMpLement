2d44dc5cf084974d1af2842147709f52bfd0bf77
geekieo <geekieo@hotmail.com>
2018-01-30 11:37:01 +0800

    commit

--- learnPy/with_test.py
+++ learnPy/with_test.py
@@ -1,13 +1,25 @@
 class Sample:
     def __enter__(self):
+        # 对象入口
         print("In __enter__()")
-        return "Foo"
+        # return "Foo"
+        return self
  
     def __exit__(self, type, value, trace):
+        # 如果对象存在异常，type, value, trace 返回异常信息
         print("In __exit__()")
+        print("type:", type)
+        print("value:", value)
+        print("trace:", trace)
+
+    def do_something(self):
+        bar = 1/0 #此处应判别为除0异常
+        return bar + 10
  
 def get_sample():
     return Sample()
- 
+
+# 将 Sample().__enter__() 的返回值赋予 sample
 with get_sample() as sample:
-    print("sample:", sample)
+    # print("sample:", sample)
+    sample.do_something()
