f9800a66305a578a0f4b7526315e979efb9ef55b
geekieo <geekieo@hotmail.com>
2018-03-17 21:50:46 +0800

    rename

---
+++ learnPy/Algorithms/sort/MaxPQ.py
@@ -0,0 +1,12 @@
+"""
+最大优先队列 - 基于堆的实现
+Max Priority Queue - Based on Heap
+"""
+class MaxHeap():
+    def __init__(self ):
+        self.pq     #基于堆的完全二叉树
+        self.n      #存储于pq[1..n]中，pq[0]没有使用
+    
+    def MaxPQ(self, maxN):
+        self.pq = Comparable[maxN+1]
+
