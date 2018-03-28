555085ffbcf8b464ec25c5bc83ac74f12f5069da
geekieo <geekieo@hotmail.com>
2018-03-28 13:56:14 +0800

    split data at random

--- model_tf/cats_vs_dogs/input_train_val_split.py
+++ model_tf/cats_vs_dogs/input_train_val_split.py
@@ -28,7 +28,7 @@ import os
 import math
 
 # you need to change this to your data directory
-train_dir = "D:\\Documents\\PySpace\\dataset\\cats_vs_dogs\\train\\"
+train_dir = "D://Documents//PySpace//dataset//cats_vs_dogs//train//"
 
 def get_files(file_dir, ratio):
     """
@@ -42,7 +42,7 @@ def get_files(file_dir, ratio):
     label_cats = []
     dogs = []
     label_dogs = []
-    # 解析文件名，划分正负样本
+    # analysis filename, split positive and negative samples
     with open(file_dir) as file:
         name = file.split(sep='.')
         if name[0] == 'cat':
@@ -53,7 +53,7 @@ def get_files(file_dir, ratio):
     label_dogs = [1]*len(dogs)
     print('There are %d cats\nThere are %d dogs'%len(cats),len(dogs))
 
-    # 用numpy实现样本随机划分
+    # use numpy to split samples at random
     image_list = np.hstack((cats,dogs))
     label_list = np.hstack((label_cats,label_dogs))
 
@@ -63,3 +63,17 @@ def get_files(file_dir, ratio):
 
     ran_image_list = image_label_pair[:,0]
     ran_label_list = image_label_pair[:,1]
+
+    n_sample = len(ran_image_list)  #25000
+    n_val = math.ceil(n_sample*ratio)   # number of validation set, round up to a integer
+    n_tra = n_sample - n_val #number of train set
+
+    tra_images = ran_image_list[0:n_tra]
+    tra_labels = ran_label_list[0:n_tra]
+    val_images = ran_image_list[n_tra:]
+    val_images = ran_label_list[n_tra:]
+
+    return tra_images,tra_labels,val_images,val_images
+
+if __name__ == "__main__":
+    tra_images,tra_labels,val_images,val_image=get_files(train_dir,0.2)
