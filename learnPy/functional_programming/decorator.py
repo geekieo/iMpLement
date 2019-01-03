3480883f781ef996a780cc1b31049538ced86c58
geekieo <geekieo@hotmail.com>
2019-01-03 17:39:42 +0800

    装饰器

---
+++ learnPy/functional_programming/decorator.py
@@ -0,0 +1,15 @@
+# -*- encode:utf-8 -*-
+class Foo(object):
+    def __init__(self, func):
+        self._func = func
+
+    def __call__(self):
+        print ('class decorator runing')
+        self._func()
+        print ('class decorator ending')
+
+@Foo
+def bar():
+    print ('bar')
+
+bar()
