f6d4bc773b47aa769844123e6e4cd81a8535a989
geekieo <geekieo@hotmail.com>
2019-01-18 17:29:32 +0800

    update

--- learnPy/logging/last/test.py
+++ learnPy/logging/last/test.py
@@ -2,3 +2,9 @@ import logger
 
 logging = logger.Logging(name='main', filename='logging')
 logging.debug('一个debug信息')
+
+varibale='hello logging '
+logging.info('info1 %s'
+             'info2'
+             'info3',
+             varibale)
