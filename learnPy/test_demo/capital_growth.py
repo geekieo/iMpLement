6e4ea6f7a5f36624d154df0c95f2d9e3f74eeeee
geekieo <geekieo@hotmail.com>
2023-05-18 15:53:57 +0800

    增加交叉点（拐点）判断，更改资金总量显示位数，只显示小数点后两位

--- learnPy/test_demo/capital_growth.py
+++ learnPy/test_demo/capital_growth.py
@@ -15,13 +15,24 @@ def growth(captial, capital_increase=True, rate=1.04, year=30):
         # cumulative_captial = captial * （rate ** year)
     return cumulative_captial
 
+def find_inflection(a, b, inflection_flag):
+    if inflection_flag:
+        if a>b:
+            return True
+    return False
+
 if __name__ == "__main__":
     captial_peryear =  0.68
     captial_once = 9.976
     rate = 1.04 # 年增长率以年通胀率1.043为参考
+    inflection_flag = True # 寻找拐点标记
     for year in range(30):
-        print("每年循环增资{0}，年增长率{1}，{2}年后：".format(captial_peryear, rate, year),
-            growth(captial=captial_peryear, capital_increase=True, year=year),end="\t")
-        print("一次性投资{0}，年增长率{1}，{2}年后：".format(captial_once, rate, year),
-            growth(captial=captial_once, capital_increase=False, year=year))
-        
+        peryear_final = growth(captial=captial_peryear, capital_increase=True, year=year)
+        once_final = growth(captial=captial_once, capital_increase=False, year=year)
+        if year > 0:
+            print()
+        print("每年循环增资{0}，年增长率{1}，{2}年后：{3:.2f}".format(captial_peryear, rate, year,peryear_final), end="\t")
+        print("一次性投资{0}，年增长率{1}，{2}年后：{3:.2f}".format(captial_once, rate, year, once_final), end="\t")
+        if find_inflection(peryear_final, once_final, inflection_flag):
+            print("拐点", end="\t")
+            inflection_flag = False
